8b7bdc2a1b6a4241651389cfef171a52f15f9a7b
Abdelrahman Magdi <abdomagdi300@gmail.com>
2023-08-07 22:43:05 +0200

    cycle detection

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * check_cycle - ...
+ * @list: ...
+ *
+ * Return: ...
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	while (slow != NULL && fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
