93bb23298d976a3169fdf9cd63a69c934f540c09
Abdelrahman Magdi <abdomagdi300@gmail.com>
2023-08-14 16:17:00 +0200

    header files hot fix

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <object.h>
-#include <listobject.h>
+#include <Python.h>
 
 /**
  * print_python_list_info - print python lists info.
@@ -11,7 +10,7 @@ void print_python_list_info(PyObject *p)
 	int i;
 	PyListObject *list_p = (PyListObject *)p;
 
-	printf("[*] Size of the Python List = %ld\n", list_p->ob_base.ob_size);
+	printf("[*] Size of the Python List = %ld\n", Py_SIZE(p));
 	printf("[*] Allocated = %ld\n", list_p->allocated);
 
 	for (i = 0; i < list_p->ob_base.ob_size; i++)
