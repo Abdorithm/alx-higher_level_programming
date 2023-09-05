c3ce3f1da69b223ddac180e1081de4cb0d20adfd
Abdelrahman Magdi <abdomagdi300@gmail.com>
2023-09-05 19:42:26 +0200

    indentation fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -20,48 +20,48 @@ class Rectangle:
         self.width = width
         self.height = height
 
-        @property
-        def width(self):
-            """getter of __width
-            Returns:
-                width of the rectangle
-            """
-            return self.__width
+    @property
+    def width(self):
+        """getter of __width
+        Returns:
+            width of the rectangle
+         """
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """setter of __width
-            Args:
-                value (int): the width
-            Returns:
-                None
-            """
-            if type(value) is int:
-                if value < 0:
-                    raise ValueError("width must be >= 0")
-                self.__width = value
-            else:
-                raise TypeError("width must be an integer")
+    @width.setter
+    def width(self, value):
+        """setter of __width
+        Args:
+            value (int): the width
+        Returns:
+            None
+        """
+        if type(value) is int:
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            self.__width = value
+        else:
+            raise TypeError("width must be an integer")
 
-        @property
-        def height(self):
-            """getter of __height
-            Returns:
-                height of the rectangle
-            """
-            return self.__height
+    @property
+    def height(self):
+        """getter of __height
+        Returns:
+            height of the rectangle
+        """
+        return self.__height
 
-        @height.setter
-        def width(self, value):
-            """setter of __height
-            Args:
-                value (int): the height
-            Returns:
-                None
-            """
-            if type(value) is int:
-                if value < 0:
-                    raise ValueError("height must be >= 0")
-                self.__height = value
-            else:
-                raise TypeError("height must be an integer")
+    @height.setter
+    def height(self, value):
+        """setter of __height
+        Args:
+            value (int): the height
+        Returns:
+            None
+        """
+        if type(value) is int:
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            self.__height = value
+        else:
+            raise TypeError("height must be an integer")
