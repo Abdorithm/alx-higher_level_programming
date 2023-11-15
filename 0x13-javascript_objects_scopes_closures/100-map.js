4b5e259d7bf51a9a67a217a3ada1a85f5ee6e26b
Abdelrahman Magdi <abdomagdi300@gmail.com>
2023-11-16 01:05:47 +0200

    fix

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const list = require('./100-data.js');
+const list = require('./100-data.js').list;
 const map1 = list.map((elem, idx) => elem * idx);
 console.log(list);
 console.log(map1);
