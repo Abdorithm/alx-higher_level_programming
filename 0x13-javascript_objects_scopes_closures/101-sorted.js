84a7678f220d07a3041917c710e6da6ff122c3c2
Abdelrahman Magdi <abdomagdi300@gmail.com>
2023-11-16 01:20:08 +0200

    advanced

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const dict = require('./101-data.js').dict;
+const newDict = {};
+for (const key in dict) {
+  const val = dict[key];
+  if (!newDict[val]) newDict[val] = [];
+  newDict[val].push(key);
+}
+console.log(newDict);
