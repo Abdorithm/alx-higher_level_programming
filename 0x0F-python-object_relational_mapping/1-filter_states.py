5f832b6c8214772b388714acf5b3a11ed25c6a87
Abdelrahman Magdi <abdomagdi300@gmail.com>
2023-11-19 23:14:30 +0200

    filter

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""lists all states from the database with IDs in ascending order"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    userName = sys.argv[1]
+    passwd = sys.argv[2]
+    table = sys.argv[3]
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=userName,
+                           passwd=passwd, db=table, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states like 'N%' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
