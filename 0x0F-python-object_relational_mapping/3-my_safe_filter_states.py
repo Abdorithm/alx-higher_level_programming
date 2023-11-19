f55b93d83718eb1f1a8a52bd8112b36241f4be6c
Abdelrahman Magdi <abdomagdi300@gmail.com>
2023-11-20 00:02:25 +0200

    SQLi

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,12 +8,15 @@ if __name__ == "__main__":
     userName = sys.argv[1]
     passwd = sys.argv[2]
     table = sys.argv[3]
+    state = sys.argv[4]
 
     conn = MySQLdb.connect(host="localhost", port=3306, user=userName,
                            passwd=passwd, db=table, charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name\
-                 LIKE BINARY 'N%' ORDER BY id ASC")
+    if "\'" in state or "--" in state:
+        exit()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(state))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
