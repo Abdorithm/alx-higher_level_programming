24c49d0c6034e89a7e43319281ce4dd7908b6817
Abdelrahman Magdi <abdomagdi300@gmail.com>
2023-12-02 22:22:44 +0200

    send email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" script that send a POST request with an email param,
+then displays the body of the response decoded in utf-8. """
+import requests
+import sys
+
+if __name__ == "__main__":
+    payload = {'email': sys.argv[2]}
+    r = requests.get(sys.argv[1], params=payload)
+    print(r.text)
