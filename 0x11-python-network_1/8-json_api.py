601af7443f6c2e4053cd3c40d31957d6bcf684c6
Abdelrahman Magdi <abdomagdi300@gmail.com>
2023-12-03 01:21:27 +0200

    hotter fix

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,20 +5,20 @@ import sys
 import requests
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     if len(sys.argv) == 1:
-        char = ""
+        char = ''
     else:
         char = sys.argv[1]
-    payload = {"p": char}
+    payload = {'q': char}
 
     r = requests.post('http://0.0.0.0:5000/search_user', data=payload)
 
     try:
         response = r.json()
         if response == {}:
-            print("No result")
+            print('No result')
         else:
-            print("[{}] {}".format(response.get("id"), response.get("name")))
+            print('[{}] {}'.format(response.get('id'), response.get('name')))
     except ValueError:
-        print("Not a valid JSON")
+        print('Not a valid JSON')
